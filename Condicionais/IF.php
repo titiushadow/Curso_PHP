1c2afe36430803d846ef0a6624dd9720da874b80
mesaque <mesaque1212@gmail.com>
2022-05-21 09:25:18 -0300

    If e Else exemplo

---
+++ Condicionais/IF.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+* Condicionais
+* if
+* else
+* elseif
+*/
+
+// IF =  execulta um trecho de codigo se uma condição for verdadeiro 
+$numero = 10;
+
+if ($numero == 10):
+    echo "Numero é igual a 10";
+else:
+    echo "é diferente de 10";
+endif;
+
+?>
