662e3d6b35c9018f8eab57d2c85b237ec3233f6a
mesaque <mesaque1212@gmail.com>
2022-05-21 09:24:59 -0300

    Elseif exemplo

---
+++ Condicionais/Elseif.php
@@ -0,0 +1,15 @@
+<?php
+
+// Elseif
+$numero = 50;
+
+if ($numero == 10):
+    echo "Numero é igual a 10";
+elseif($numero <= 9):
+    echo "É menor ou igual a 9";
+else:
+    echo "é diferente de 10";
+endif;
+
+
+?>
