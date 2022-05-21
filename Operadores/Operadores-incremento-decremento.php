80028e68f16ea608d1b39f9e044d70deb11dfa7c
mesaque <mesaque1212@gmail.com>
2022-05-21 10:08:48 -0300

    Incremento Decremento

---
+++ Operadores/Operadores-incremento-decremento.php
@@ -0,0 +1,35 @@
+<?php
+
+//  Operadores de incremento e decremento
+// Servem para somar ou subtrair em (+1 ou -1)
+// qualquer valor do tipo numérico ou string.
+
+// Incremento
+$valor = 20;
+
+//Pré incremento
+echo ++$valor;
+echo "<hr>";
+
+// Pós-incremento
+echo $valor++;
+echo "<br>";
+echo $valor;
+echo "<hr>";
+
+////////////////////////
+
+//Decremento
+
+//Pré-decremento
+echo --$valor;
+echo "<hr>";
+
+//Pós-drecremento
+echo $valor--;
+echo "<br>";
+echo $valor;
+
+
+
+?>
