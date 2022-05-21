774ab29c7a8025dbbf97190aa8725ad1d1effdef
mesaque <mesaque1212@gmail.com>
2022-05-20 23:09:13 -0300

    Aspas simples e concatenacao

---
+++ Aspas_concatenacao/Aspas-concatenacao.php
@@ -0,0 +1,11 @@
+<?php
+
+$nome = 'Mesaque';
+$idade = '18';
+echo 'Meu nome é '.$nome. ' e minha idade é \'18\''; // Tudo que estiver em aspas simples dentro do echo é um texto
+
+echo '<hr>';
+
+echo "Meu nome é \"$nome\"";
+
+?>
