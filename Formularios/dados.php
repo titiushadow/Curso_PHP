d942d69d699b8a845a1d5c286a383e25e44e514c
mesaque <mesaque1212@gmail.com>
2022-05-21 15:05:53 -0300

    Testando $_GET

--- Formularios/dados.php
+++ Formularios/dados.php
@@ -1,9 +1,6 @@
 <?php
 
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-
-echo "Seu nome é $nome<br> e seu email é $email";
-// var_dump($_POST);
+echo $_GET['idade']."<br>".$_GET['sobrenome'];
+// var_dump($_GET);
 
 ?>
