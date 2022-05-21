f4e30ee955ab956733d17dd77284a420f9c3f764
mesaque <mesaque1212@gmail.com>
2022-05-20 21:52:50 -0300

    Declarando_variaveis

---
+++ Variaveis/variaveis.php
@@ -0,0 +1,40 @@
+<?php
+//Meus dados
+$nome = "Mesaque";
+$idade = 18;
+$altura = 1.73;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura <br>";
+
+// Alterando meus dados
+$nome = "Meseikers";
+$idade = 22;
+$altura = 1.80;
+
+echo "Meu nome é $nome, minha idade é $idade e minha altura é $altura";
+echo "<hr>";
+
+$carro = "Civic";
+$Carro = "Hilux";
+
+$meunome = "Mesaque";
+$meuNome = "Meseikers";
+
+echo $Carro;
+echo "<br>";
+echo $meuNome;
+
+// Nomes válidados
+// $nome
+// $_meunome
+// $carro10
+// $nota1
+// $meu_nome
+// $meu_nome
+
+// Nomes inválidos
+// $123456nome
+// $carro$#$%%
+// $meu carro
+
+?>
