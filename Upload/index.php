718dfdb26e7237b87c1a6d0602a6b7b5139f87cd
mesaque <mesaque1212@gmail.com>
2022-05-22 15:29:58 -0300

    Upload de varios arquivos

--- Upload/index.php
+++ Upload/index.php
@@ -7,33 +7,39 @@
     <title>Upload de arquivos</title>
 </head>
 <body>
+
     <?php
         if(isset($_POST['enviar-formulario'])):
             $formatosPermitidos = array("png", "jpeg", "jpg", "gif");
-            $extensao = pathinfo($_FILES['arquivo']['name'], PATHINFO_EXTENSION);
-            
-            if(in_array($extensao, $formatosPermitidos)):
-                $pasta = "arquivos/";
-                $temporario = $_FILES['arquivo']['tmp_name'];
-                $novoNome = uniqid().".$extensao";
+            $quantidadeArquivos = count($_FILES['arquivo']['name']);
+            $contador = 0;
 
-                if(move_uploaded_file($temporario, $pasta.$novoNome)):
-                    $mensagem = "Upload feito com sucesso!";
+            while ($contador < $quantidadeArquivos):
+
+                $extensao = pathinfo($_FILES['arquivo']['name'][$contador], PATHINFO_EXTENSION);
+                
+                if(in_array($extensao, $formatosPermitidos)):
+                    $pasta = "arquivos/";
+                    $temporario = $_FILES['arquivo']['tmp_name'][$contador];
+                    $novoNome = uniqid().".$extensao";
+
+                    if(move_uploaded_file($temporario, $pasta.$novoNome)):
+                        echo "Upload feito com sucesso para $pasta$novoNome<br>";
+                    else:
+                        echo "Erro ao enviar o arquivo $temporario";
+                    endif;
                 else:
-                    $mensagem = "Erro não foi possivel fazer o upload";
+                    echo "$extensao não é permitida <br>";
                 endif;
-            else:
-            $mensagem = "Formato Invalido";
-            endif;
 
-            echo  $mensagem;
-
+                $contador++;
+            endwhile;
         endif;
     ?>
 
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST"
     enctype="multipart/form-data">
-        <input type="file" name="arquivo"><br>
+        <input type="file" name="arquivo[]" multiple><br>
         <input type="submit" name="enviar-formulario">
     </form>
     
