59e46d391e026543e30cc36efeb299147ef62ea7
mesaque <mesaque1212@gmail.com>
2022-05-22 20:21:49 -0300

    proteção sql e xss

--- Crud/php_action/create.php
+++ Crud/php_action/create.php
@@ -5,11 +5,21 @@ session_start();
 // Conexão
 require_once 'db_connect.php';
 
+// Clear
+function clear($input) {
+    global $connect;
+    // sql
+    $var = mysqli_escape_string($connect, $input);
+    // xss
+    $var = htmlspecialchars($var);
+    return $var;
+}
+
 if(isset($_POST['btn-cadastrar'])):
-    $nome = mysqli_escape_string($connect, $_POST['nome']);
-    $sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-    $email = mysqli_escape_string($connect, $_POST['email']);
-    $idade = mysqli_escape_string($connect, $_POST['idade']);
+    $nome = clear($_POST['nome']);
+    $sobrenome = clear($_POST['sobrenome']);
+    $email = clear($_POST['email']);
+    $idade = clear($_POST['idade']);
 
     $sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome', '$sobrenome', '$email', '$idade')";
 
